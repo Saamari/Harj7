9dcf0a1037af5ec6b2807de70a8f54fb3ab4f785
Saamari <sampsa_kautto@hotmail.com>
2016-01-13 15:24:49 +0200

    Harj7

---
+++ Harj7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+
+            Console.WriteLine("Anna Vuosi");
+            year = int.Parse(Console.ReadLine());
+
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                Console.WriteLine("On Karkausvuosi");
+            }
+
+            else
+                Console.WriteLine("Ei ole karkausvuosi");
+                    
+                
+        }
+    }
+}
